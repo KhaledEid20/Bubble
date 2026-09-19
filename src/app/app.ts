bc93553cbfd5ff4605fde71ff7605a505bf09b47
Khaled_Eid <khaled_eid77@outlook.com>
2026-09-19 21:12:56 +0300

    FolderStructure & initial Configuration

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +9,8 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Bubble');
+export class App implements OnInit {
+  ngOnInit(): void {
+    initFlowbite();
+  }
 }
